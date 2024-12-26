c92c222bc363566f7af2435898c7b6d822f3f6c3
gabrielabade <gabriela.bade@hotmail.com>
2024-12-26 15:57:16 -0300

    I created a component folder, starting with the header all navbar items

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
-import logo from './assets/images/logo.svg';
-
+import './styles/App.css';
+import Navbar from './components/Navbar/Navbar';
 class App extends React.Component {
   render() {
-    return (
-      <header>
-        <nav id='navbar'>
-          <div className="nav-brand">
-              <img src={logo} alt="" />
-              <h1>Space Flight News</h1>
-          </div>
-          <ul className="navlist">
-            <li><a href='/'>Home</a></li>
-            <li><a href='/'>Trending</a></li>
-            <li><a href='/'>Categories</a></li>
-            <li><a href='/'>About</a></li>
-            <li><a href='/'>Contact</a></li>
-          </ul>
-        </nav>
-      </header>
-    );
+    return <Navbar />;
+
   }
 }
 
